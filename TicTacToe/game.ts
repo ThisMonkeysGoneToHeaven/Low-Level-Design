d066b9b87828bc4dc3291cbcdeb4fc065825e495
ThisMonkeysGoneToHeaven <anantdiagarwal@gmail.com>
2024-06-06 13:51:54 +0530

    made minor progress

---
+++ TicTacToe/game.ts
@@ -0,0 +1,21 @@
+enum BOARD_STATUS{
+    PLAYER_1_WINS,
+    PLAYER_2_WINS,
+    DRAW,
+    UNDECIDED
+}
+
+enum PIECES{
+    ZERO,
+    CROSS
+}
+
+enum CELL_VALUES{
+    EMPTY,
+    ZERO,
+    CROSS
+}
+
+/*
+
+*/
