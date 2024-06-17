a1c904264b99f0bd45970e8d809c0db9699d43ae
ThisMonkeysGoneToHeaven <anantdiagarwal@gmail.com>
2024-06-17 08:10:47 +0530

    made board a private entity

--- TicTacToe/Board.ts
+++ TicTacToe/Board.ts
@@ -6,7 +6,7 @@ enum BOARD_VALS{
 
 class Board{
 
-    board: BOARD_VALS[][];
+    private board: BOARD_VALS[][];
 
     constructor(board_size: number){
         this.board = Array.from(Array(board_size), () => Array(board_size).fill(BOARD_VALS.EMPTY));
